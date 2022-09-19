6fef3e5684719e1650b281d7e8a037e35d414e81
Ashley <dresswithpockets@pm.me>
2022-09-19 03:11:28 -0700

    add radial camera collision script

---
+++ src/Game/RadialCamera.cs
@@ -0,0 +1,35 @@
+﻿
+using Godot;
+
+public class RadialCamera : Spatial
+{
+    [Export] public NodePath Target;
+    [Export] public Vector3 TargetOffset;
+    [Export] public Shape Shape;
+    [Export(PropertyHint.Layers3dPhysics)] public uint CollisionMask;
+
+    private Camera _camera;
+
+    public override void _Ready()
+    {
+        this.TryGetChild(out _camera);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        var space = GetWorld().DirectSpaceState;
+        var targetPos = GetNode<Spatial>(Target).GlobalTransform.origin + TargetOffset;
+        var motion = GlobalTransform.origin - targetPos;
+        var query = new PhysicsShapeQueryParameters
+        {
+            Margin = Shape.Margin,
+            CollisionMask = CollisionMask,
+            Transform = new Transform(Basis.Identity, targetPos),
+        };
+        query.SetShape(Shape);
+        var results = space.CastMotion(query, motion);
+        var safeFraction = (float)results[0];
+        var newCameraPos = targetPos.Lerp(GlobalTransform.origin, safeFraction);
+        _camera.GlobalTranslation = newCameraPos;
+    }
+}
