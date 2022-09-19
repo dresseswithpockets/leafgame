bcaee0115454641bcd15602061c9ab87d38c6338
Ashley <dresswithpockets@pm.me>
2022-09-19 03:11:17 -0700

    add helper extensions

--- src/Game/TreeExtensions.cs
+++ src/Game/TreeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using Godot;
 
 public static class TreeExtensions
@@ -13,5 +14,31 @@ public static class TreeExtensions
     public static SignalAwaiter AwaitNextProcess(this Node node, bool processDuringPause = true)
         => AwaitTimer(node, 0f, processDuringPause);
 
-    
+    public static bool TryGetChild<T>(this Node node, out T child)
+    {
+        foreach (var c in node.GetChildren())
+        {
+            if (!(c is T t)) continue;
+            
+            child = t;
+            return true;
+        }
+
+        child = default;
+        return false;
+    }
+
+    public static IEnumerable<T> GetChildren<T>(this Node node)
+    {
+        foreach (var c in node.GetChildren())
+        {
+            if (!(c is T t)) continue;
+            yield return t;
+        }
+    }
+
+    public static Vector3 Lerp(this Vector3 a, Vector3 b, float t) => new Vector3(
+        Mathf.Lerp(a.x, b.x, t),
+        Mathf.Lerp(a.y, b.y, t),
+        Mathf.Lerp(a.z, b.z, t));
 }
