c6c4a15e4fba3191bfba9f623cd58404ab6b1f68
Ashley <dresswithpockets@pm.me>
2022-09-21 01:43:47 -0700

    add wind rings for flight

---
+++ src/WindRing/WindRing.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class WindRing : Spatial
+{
+    [Export] public float Cooldown = 5;
+    
+    private Area _area;
+    private float _cooldownTimer;
+
+    public override void _Ready()
+    {
+        _area = GetNode<Area>("Area");
+        _area.Connect("body_entered", this, nameof(OnBodyEntered));
+    }
+
+    public override void _Process(float delta)
+    {
+        _cooldownTimer = Mathf.Clamp(_cooldownTimer - delta, 0, Cooldown);
+    }
+
+    private void OnBodyEntered(Node body)
+    {
+        GD.Print($"{body.Name} entered");
+        if (!(body is PlayerLeaf player)) return;
+        if (_cooldownTimer > 0) return;
+
+        player.AddGlideBoost();
+        _cooldownTimer = Cooldown;
+    }
+}
