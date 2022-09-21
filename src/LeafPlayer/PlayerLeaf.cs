4c66a61d0e95bb6479f47de221b7b4b80c4494ea
Ashley <dresswithpockets@pm.me>
2022-09-21 01:43:20 -0700

    add gliding movement

--- src/LeafPlayer/PlayerLeaf.cs
+++ src/LeafPlayer/PlayerLeaf.cs
@@ -1,25 +1,199 @@
 using Godot;
-using System;
 
 public class PlayerLeaf : KinematicBody
 {
     [Export] public float Gravity = -4;
-    [Export] public float MaxSpeed = 4;
-    [Export] public float Accel = 30;
-    [Export] public float Decel = -10;
+    [Export] public float MaxGroundSpeed = 4;
+    [Export] public float GroundAccel = 30;
+    [Export] public float GroundDecel = -10;
 
+    [Export] public float MaxGlidePower = 5;
+    [Export] public float MinGlidePower = 1;
+    [Export] public float GlidePowerDecelRate = 3;
+    /// <summary>
+    /// amount of time after getting a power boost before deceleration kicks in
+    /// </summary>
+    [Export] public float GlidePowerDecelCooldown = 1;
+
+    /// <summary>
+    /// the amount of time the player can be at the min glide power before they lose their glide status
+    /// </summary>
+    [Export] public float GlideAtMinBeforeDecayTime = 3;
+
+    [Export] public float MinCameraDistance = 1;
+    [Export] public float MaxCameraDistance = 2;
+
+    [Export] public float GlideMinPitch = -0.5f;
+    [Export] public float GlideMaxPitch = 0.5f;
+    [Export] public float GlideYawSpeed = 1f;
+    [Export] public float GlidePitchSpeed = 0.25f;
+
+    [Export] public NodePath GroundCamera;
+    [Export] public NodePath GroundCameraOrbiter;
+    [Export] public NodePath GlideCameraFollow;
+    [Export] public NodePath GlideCamera;
+
+    private Spatial _groundCameraOrbiter;
+    private Spatial _groundCamera;
+    private Spatial _glideCameraFollow;
+    private Spatial _glideCamera;
+    private bool _gliding;
     private Vector3 _wishDir;
+    
     private float _verticalSpeed;
     private Vector3 _horizontalVelocity;
+
+    private float _glidePitch;
+    private float _glideYaw;
+    private Vector3 _glideVelocity;
+    private float _glidePower;
+    private float _glideCooldownTimer;
+    private float _glideMinTimer;
+
+    public Vector3 Velocity => _gliding ? _glideVelocity : _horizontalVelocity;
     
     public override void _Ready()
     {
-        
+        _groundCamera = GetNode<Spatial>(GroundCamera);
+        _glideCameraFollow = GetNode<Spatial>(GlideCameraFollow);
+        _glideCamera = GetNode<Spatial>(GlideCamera);
+        _glidePower = MinGlidePower;
+        _groundCameraOrbiter = GetNode<Spatial>(GroundCameraOrbiter);
     }
 
-    public override void _Process(float delta)
+    public void AddGlideBoost()
     {
+        if (!_gliding)
+        {
+            _glideCameraFollow.GlobalTransform = _groundCamera.GlobalTransform;
+            _glideVelocity = _horizontalVelocity;
+            _glideCamera.Set("enabled", true);
+            var glideEuler = _groundCamera.GlobalTransform.basis.GetEuler();
+            _glidePitch = glideEuler.x;
+            _glideYaw = glideEuler.y;
+        }
+
+        _gliding = true;
+        _glideCooldownTimer = 0;
+        _glideMinTimer = 0;
+        _glidePower = MaxGlidePower;
+    }
+
+    private void MoveGround(float delta)
+    {
+        // move horizontally relative to our camera
+        _wishDir = GetViewport().GetCamera().GlobalTransform.basis.Xform(_wishDir).Normalized();
         
+        var offset = (_wishDir * GroundAccel * MaxGroundSpeed * delta) + (_horizontalVelocity.Normalized() * GroundDecel * MaxGroundSpeed * delta);
+
+        if (_wishDir == Vector3.Zero && offset.LengthSquared() > _horizontalVelocity.LengthSquared())
+            _horizontalVelocity = Vector3.Zero;
+        else
+        {
+            _horizontalVelocity.x = Mathf.Clamp(_horizontalVelocity.x + offset.x, -MaxGroundSpeed, MaxGroundSpeed);
+            _horizontalVelocity.z = Mathf.Clamp(_horizontalVelocity.z + offset.z, -MaxGroundSpeed, MaxGroundSpeed);
+
+            if (IsOnFloor())
+                _verticalSpeed = 0f;
+            else
+                _verticalSpeed += Gravity * delta;
+            
+            HandleGroundMoveAndSlide();
+        }
+
+        foreach (var child in this.GetChildren<IPlayerEvents>())
+            child.GroundSpeed(_horizontalVelocity.Length(), MaxGroundSpeed);
+    }
+
+    private void HandleGroundMoveAndSlide()
+    {
+        var vel = _horizontalVelocity;
+        vel.y = _verticalSpeed;
+        vel = MoveAndSlideWithSnap(vel, new Vector3(0, -0.1f, 0f), Vector3.Up, true);
+            
+        _horizontalVelocity = vel;
+        _verticalSpeed = _horizontalVelocity.y;
+        _horizontalVelocity.y = 0f;
+    }
+
+    private void MoveGlide(float delta)
+    {
+        if (_wishDir != Vector3.Zero)
+        {
+            _glidePitch += _wishDir.z * GlidePitchSpeed * Mathf.Pi * delta;
+            _glidePitch = Mathf.Clamp(_glidePitch, GlideMinPitch, GlideMaxPitch);
+            _glideYaw += -_wishDir.x * GlideYawSpeed * Mathf.Pi * delta;
+            _glideVelocity = new Basis(new Vector3(_glidePitch, _glideYaw, 0f)).Xform(Vector3.Forward) * _glidePower;
+        }
+
+        // clamp our velocity to our current glide power
+        _glideVelocity = _glideVelocity.Normalized() * _glidePower;
+
+        if (_glideCooldownTimer < GlidePowerDecelCooldown)
+            _glideCooldownTimer += delta;
+        
+        if (_glideCooldownTimer >= GlidePowerDecelCooldown)
+            _glidePower -= GlidePowerDecelRate * delta;
+
+        if (_glidePower <= MinGlidePower)
+        {
+            _glidePower = MinGlidePower;
+            _glideMinTimer += delta;
+        }
+        else
+        {
+            _glideMinTimer = 0;
+        }
+
+        // if we've decayed our gliding state, then immediately process a ground move 
+        if (_glideMinTimer >= GlideAtMinBeforeDecayTime)
+        {
+            foreach (var child in this.GetChildren<IPlayerGlidingEvent>())
+                child.GlideOver();
+            
+            _glideCamera.Set("enabled", false);
+            _groundCameraOrbiter.Set("input_rotation",
+                new Vector2(Mathf.Rad2Deg(_glideYaw), Mathf.Rad2Deg(_glidePitch)));
+            _gliding = false;
+            _horizontalVelocity = _glideVelocity;
+            _verticalSpeed = _horizontalVelocity.y;
+            
+            // todo: maybe this isnt necessary
+            HandleGroundMoveAndSlide();
+            return;
+        }
+
+        _glideVelocity = MoveAndSlide(_glideVelocity, Vector3.Up);
+        
+        // adjust our camera to always be behind our player after moving, based on the direction they're moving
+        var inverseCameraDir = -_glideVelocity.Normalized();
+        var glideCameraTransform = _glideCameraFollow.Transform;
+        var glidePowerRatio = (_glidePower - MinGlidePower) / (MaxGlidePower - MinGlidePower);
+        glideCameraTransform.origin =
+            inverseCameraDir * Mathf.Lerp(MinCameraDistance, MaxCameraDistance, glidePowerRatio);
+        _glideCameraFollow.Transform = glideCameraTransform;
+        
+        // grab our corrected pitch and yaw after moving
+        {
+            var lookAt = new Transform().LookingAt(_glideVelocity, Vector3.Up);
+            var lookAtEuler = lookAt.basis.GetEuler();
+            _glidePitch = lookAtEuler.x;
+            _glideYaw = lookAtEuler.y;
+            /*var yawVel = _glideVelocity;
+            yawVel.y = 0f;
+            _glideYaw = Vector3.Forward.AngleTo(yawVel.Normalized());
+
+            var pitchVel = new Vector3(0f, _glideVelocity.y, 0f);
+            _glidePitch = Mathf.Clamp(Vector3.Forward.AngleTo(pitchVel.Normalized()), GlideMinPitch, GlideMaxPitch);*/
+        }
+
+        foreach (var child in this.GetChildren<IPlayerGlidingEvent>())
+            child.Gliding(this,
+                _glidePower, 
+                new Quat(new Vector3(_glidePitch, _glideYaw, 0f)),
+                _glideVelocity,
+                _glideCooldownTimer >= GlidePowerDecelCooldown,
+                _glideMinTimer >= GlideAtMinBeforeDecayTime);
     }
 
     public override void _PhysicsProcess(float delta)
@@ -30,34 +204,21 @@ public class PlayerLeaf : KinematicBody
             _wishDir = new Vector3(Input.GetActionStrength("MoveRight") - Input.GetActionStrength("MoveLeft"),
                 0f,
                 Input.GetActionStrength("MoveBackward") - Input.GetActionStrength("MoveForward"));
-
-            _wishDir = GetViewport().GetCamera().GlobalTransform.basis.Xform(_wishDir).Normalized();
         }
-
-        var offset = (_wishDir * Accel * MaxSpeed * delta) + (_horizontalVelocity.Normalized() * Decel * MaxSpeed * delta);
-
-        if (_wishDir == Vector3.Zero && offset.LengthSquared() > _horizontalVelocity.LengthSquared())
-            _horizontalVelocity = Vector3.Zero;
+        
+        if (_gliding)
+        {
+            MoveGlide(delta);
+        }
         else
         {
-            _horizontalVelocity.x = Mathf.Clamp(_horizontalVelocity.x + offset.x, -MaxSpeed, MaxSpeed);
-            _horizontalVelocity.z = Mathf.Clamp(_horizontalVelocity.z + offset.z, -MaxSpeed, MaxSpeed);
-
-            if (IsOnFloor())
-                _verticalSpeed = 0f;
-            else
-                _verticalSpeed += Gravity * delta;
-            
-            var vel = _horizontalVelocity;
-            vel.y = _verticalSpeed;
-            vel = MoveAndSlide(vel, Vector3.Up, true);
-            
-            _horizontalVelocity = vel;
-            _verticalSpeed = _horizontalVelocity.y;
-            _horizontalVelocity.y = 0f;
+            MoveGround(delta);
         }
+    }
 
-        foreach (var child in this.GetChildren<IPlayerEvents>())
-            child.Speed(_horizontalVelocity.Length(), MaxSpeed);
+    public void HitWindWall(WindWall windWall)
+    {
+        GD.Print(windWall);
+        // todo: move towards windWall.IslandCenter
     }
 }
