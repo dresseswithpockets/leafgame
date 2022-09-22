ec2bd6bc129279f270a1d094fbdeb6506acc02fa
Ashley <dresswithpockets@pm.me>
2022-09-22 04:41:28 -0700

    dont move overlay camera if not flagged

--- src/Dialogue/UIOverlayCamera.cs
+++ src/Dialogue/UIOverlayCamera.cs
@@ -4,16 +4,18 @@ using System;
 public class UIOverlayCamera : Camera
 {
     private Camera _mainCamera;
+    private Game _game;
 
     public override void _Ready()
     {
-        var game = GetNode("%Game");
-        _mainCamera = game?.GetNode<Camera>("VCameraBrain");
+        _game = GetNode<Game>("%Game");
+        _mainCamera = _game?.GetNode<Camera>("VCameraBrain");
     }
 
     public override void _Process(float delta)
     {
         if (_mainCamera == null) return;
+        if (!_game.ShouldMoveOverlayCamera) return;
         GlobalTransform = _mainCamera.GlobalTransform;
         Fov = _mainCamera.Fov;
     }
