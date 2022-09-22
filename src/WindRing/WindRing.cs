4c483a9a61cbbd9da42904c731414300325d677b
Ashley <dresswithpockets@pm.me>
2022-09-22 11:37:59 -0700

    add wind follow support to windring

--- src/WindRing/WindRing.cs
+++ src/WindRing/WindRing.cs
@@ -5,14 +5,19 @@ public class WindRing : Spatial
 {
     [Export] public float Cooldown = 5;
     [Export] public bool Enabled = true;
+    [Export] public bool TriggerWindPath;
+    [Export] public NodePath PathFollower;
     
     private Area _area;
     private float _cooldownTimer;
+    private WindFollow _pathFollow;
 
     public override void _Ready()
     {
         _area = GetNode<Area>("Area");
         _area.Connect("body_entered", this, nameof(OnBodyEntered));
+        if (!(PathFollower?.IsEmpty() ?? true))
+            _pathFollow = GetNode<WindFollow>(PathFollower);
     }
 
     public override void _Process(float delta)
@@ -27,6 +32,12 @@ public class WindRing : Spatial
         if (!(body is PlayerLeaf player)) return;
         if (_cooldownTimer > 0) return;
 
+        if (TriggerWindPath && _pathFollow != null)
+        {
+            player.FollowWind(_pathFollow);
+            return;
+        }
+        
         player.AddGlideBoost();
         _cooldownTimer = Cooldown;
     }
