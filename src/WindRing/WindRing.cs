4790f51081acf547f38c7fbbf9a091e0a39d362f
Ashley <dresswithpockets@pm.me>
2022-09-22 04:37:16 -0700

    add enabled flag for each windring

--- src/WindRing/WindRing.cs
+++ src/WindRing/WindRing.cs
@@ -4,6 +4,7 @@ using System;
 public class WindRing : Spatial
 {
     [Export] public float Cooldown = 5;
+    [Export] public bool Enabled = true;
     
     private Area _area;
     private float _cooldownTimer;
@@ -22,6 +23,7 @@ public class WindRing : Spatial
     private void OnBodyEntered(Node body)
     {
         GD.Print($"{body.Name} entered");
+        if (!Enabled) return;
         if (!(body is PlayerLeaf player)) return;
         if (_cooldownTimer > 0) return;
 
