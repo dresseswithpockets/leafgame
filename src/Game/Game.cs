6ba4e75bc195ebbdf20a824bef796fdb6b2e43cb
Ashley <dresswithpockets@pm.me>
2022-09-22 04:41:15 -0700

    add dialogue sequencing

--- src/Game/Game.cs
+++ src/Game/Game.cs
@@ -3,50 +3,85 @@ using System;
 
 public class Game : Spatial
 {
+    public static Game Instance;
+
+    public bool ShouldMoveOverlayCamera;
+    public Camera CameraBrain;
+    
+    private PlayerSpatial _playerSpatial;
     private Spatial _gameStartCamera;
-    private Camera _cameraBrain;
     private TreeSway _tree;
     private AnimationPlayer _branchAnimator;
     private AnimationPlayer _introPlayer;
-    private AnimationPlayer _house1Player;
-    private Dialogue _house1Dialogue;
+    //private Dialogue _house1Dialogue;
+    [Export] private string _house1DialogueSequenceName;
     [Export]
     private string _branchSwayAnimationName = "BranchSway";
 
     [Signal]
     public delegate void BeginPlay();
 
+    public override void _EnterTree()
+    {
+        Instance = this;
+    }
+
     public override void _Ready()
     {
+        _playerSpatial = GetNode<PlayerSpatial>("%PlayerSpatial");
         _gameStartCamera = GetNode<Spatial>("%VCamera GameStart");
-        _cameraBrain = GetNode<Camera>("%VCameraBrain");
+        CameraBrain = GetNode<Camera>("%VCameraBrain");
         _tree = GetNode<TreeSway>("%Tree");
         _branchAnimator = GetNode<AnimationPlayer>("%BranchAnimator");
         _introPlayer = GetNode<AnimationPlayer>("%IntroPlayer");
-        _house1Player = GetNode<AnimationPlayer>("%House1Player");
-        _house1Dialogue = GetNode<Dialogue>("%House1Dialogue");
+        //_house1Dialogue = GetNode<Dialogue>("%House1Dialogue");
 
         _introPlayer.Connect("animation_finished", this, nameof(StartHouseSequence));
+        
+        // if allow input is already set to true, then assume that we want to debug the player
+        if (InputController.Instance.AllowInput)
+            _playerSpatial.GiveControl();
     }
 
     public void StartGame()
     {
+        InputController.Instance.CanLock = true;
         EmitSignal(nameof(BeginPlay));
     }
 
     public async void StartHouseSequence(string _)
     {
         // begins the house dialogue sequence and other character animations
-        // _house1Player.Play("Greeting");
+        _playerSpatial.PauseControl();
         await this.AwaitTimer(1f);
-        _house1Dialogue.StartPage();
-        var dialogueFinished = ToSignal(_house1Dialogue, nameof(Dialogue.FinishedBook));
+        //_house1Dialogue.StartPage();
+        Main.Instance.SetupDialogue(_house1DialogueSequenceName);
+        Main.Instance.StartPage();
+        var dialogueFinished = ToSignal(Main.Instance, nameof(Dialogue.FinishedBook));
+        Spatial lastCamera = null;
         while (!dialogueFinished.IsCompleted)
         {
             if (Input.IsActionJustPressed("SkipDialogue"))
-                _house1Dialogue.ContinuePage();
+            {
+                // ShouldMoveOverlayCamera = true;
+                lastCamera?.Set("enabled", false);
+                var page = Main.Instance.ContinuePage();
+                if (page?.EnableCamera ?? false)
+                {
+                    // ShouldMoveOverlayCamera = false;
+                    lastCamera = GetNode<Spatial>(page.CameraName);
+                    lastCamera.Set("enabled", true);
+                }
+            }
+
             await this.AwaitNextProcess();
         }
+
+        lastCamera?.Set("enabled", false);
+        // ShouldMoveOverlayCamera = true;
+        
+        _playerSpatial.SetCameraToCurrentCameraRotation();
+        _playerSpatial.GiveControl();
     }
 
     public override void _Process(float delta)
@@ -68,7 +103,7 @@ public class Game : Spatial
     private SignalAwaiter EaseToStartCamera()
     {
         var transitionTime = (float)_gameStartCamera.Get("transition_time");
-        var currentTarget = (Spatial)_cameraBrain.Call("get_highest_priority_vcamera");
+        var currentTarget = (Spatial)CameraBrain.Call("get_highest_priority_vcamera");
         var targetPriority = (int)currentTarget.Get("priority");
         _gameStartCamera.Set("priority", targetPriority + 1);
         return this.AwaitTimer(transitionTime);
