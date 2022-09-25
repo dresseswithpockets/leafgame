6ef30633b2db9d4521010158aec7e345a31d654d
Ashley <dresswithpockets@pm.me>
2022-09-25 15:42:44 -0700

    add deathplane script

---
+++ src/Game/DeathPlane.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+public class DeathPlane : Area
+{
+    public override void _Ready()
+    {
+        Connect("body_entered", this, nameof(OnBodyEntered));
+    }
+
+    private void OnBodyEntered(Node node)
+    {
+        if (!(node is PlayerLeaf playerLeaf)) return;
+        Game.Instance.HandleBottomPlaneHit();
+    }
+}
